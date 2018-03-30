7aadbf8d1db9891cb198910ab577cb6b55e690e7
laaptu <sanimap@gmail.com>
2018-03-30 17:15:21 +1100

    Creating first level design for listing

--- app/src/main/java/au/com/sentia/test/widgets/IconicTextView.kt
+++ app/src/main/java/au/com/sentia/test/widgets/IconicTextView.kt
@@ -5,7 +5,7 @@ import android.util.AttributeSet
 import android.widget.TextView
 import au.com.sentia.test.Injection
 
-class CustomFontTV : TextView {
+class IconicTextView : TextView {
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
